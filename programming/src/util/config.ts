f2fb9541fc235739cb1579f5b016b26d554305f8
JItrapon <jitrapon.p@entronica.co.th>
2021-02-08 21:18:59 +0700

    clean config

--- programming/src/util/config.ts
+++ programming/src/util/config.ts
@@ -1,11 +1,3 @@
-// const config = {
-//     host: process.env.DB_HOST || 'ggdbtest.cmq0ofvvelm5.us-east-2.rds.amazonaws.com',
-//     port: process.env.DB_PORT || 3306,
-//     user: process.env.DB_USER || 'admin',
-//     password: process.env.DB_PASS || 'Jitrapon1764',
-//     database: process.env.DB_NAME || 'CCCRYPT'
-// }
-
 const config = {
     host: process.env.DB_HOST ,
     port: process.env.DB_PORT ,
@@ -13,7 +5,6 @@ const config = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME 
 }
-
 
 module.exports = config
 
