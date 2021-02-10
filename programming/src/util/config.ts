391f63e36bf42a0c3b4ee675dab821cfc2307cb8
Jitrapon1764 <jitraponpoonsuk@gmail.com>
2021-02-10 22:24:12 +0700

    fix db connect and config

--- programming/src/util/config.ts
+++ programming/src/util/config.ts
@@ -5,7 +5,8 @@ const config = {
     port: process.env.DB_PORT ,
     user: process.env.DB_USER ,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME 
+    database: process.env.DB_NAME ,
+    connectionLimit: process.env.DB_CONNECTLIMIT,
 }
 
 module.exports = config
--- programming/src/util/db.ts
+++ programming/src/util/db.ts
@@ -1,6 +1,9 @@
 import { Logger } from './logger';
 const mariadb = require('mariadb');
-const config  = require('./config')
+const config = require('./config')
+
+var pool: any;
+var connect:any;
 export class MariaDB {
 
     async startDB() {
@@ -17,16 +20,16 @@ export class MariaDB {
     }
 
     async getConnection() {
-        let connect: any;
-        let pool: any;
         try {
-            pool = await mariadb.createPool(config);
-            connect = await pool.getConnection("CCCRYPT");
-            return connect
+            if(!pool && !connect){
+                pool = await mariadb.createPool(config);
+                connect = await pool.getConnection("CCCRYPT");
+                return connect
+            } else if (connect){
+                return connect
+            }
         } catch (error) {
             throw error
-        } finally {
-            if (connect) await pool.end()
         }
     }
 }
